f5a878fe67e719ab89cdbdbbcdb5351da2a62f6f
thisisnikhar <nikharsachdeva14@gmail.com>
2026-05-09 12:22:12 +0530

    Add get bucket details endpoint

--- s3/service.py
+++ s3/service.py
@@ -1,4 +1,6 @@
 import boto3
+from jinja2.bccache import Bucket
+from zoneinfo import ZoneInfo
 
 s3 = boto3.client("s3")
 s3_resource = boto3.resource("s3")
@@ -60,3 +62,30 @@ def delete_specific_bucket_service(bucket_name):
     except Exception as e:
         print(e)
         return "error",f"Error while deleting the bucket - {bucket_name}"
+
+
+def get_bucket_details_service(bucket_name):
+    try:
+        bucket = s3_resource.Bucket(bucket_name)
+        print("-"*100)
+        print(bucket)
+        created_on_utc = bucket.creation_date
+        # Converting time zone from utc to India
+        created_on_mumbai = created_on_utc.astimezone(
+            ZoneInfo("Asia/Kolkata")
+        )
+        created_on = created_on_mumbai.isoformat()
+        region = s3.get_bucket_location(Bucket=bucket_name).get("LocationConstraint")
+        # print(f"location: {location}, created_at: {created_at}")
+        # return "success","test"
+        print(created_on, region)
+        # return  "success",""
+        data = {
+            "name": bucket_name,
+            "created_on": created_on,
+            "region": region,
+        }
+        return "success",data
+    except Exception as e:
+        print(e)
+        return "error", f"Error while fetching the bucket details - {bucket_name}"
