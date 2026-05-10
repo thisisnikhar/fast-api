b2bfe8c8aec697641e444cd7d0e4aa0b798140cd
thisisnikhar <nikharsachdeva14@gmail.com>
2026-05-10 14:07:31 +0530

    Implemented launch_0instance_service for ec2

--- ec2/routes.py
+++ ec2/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from ec2.service import get_instances_service
+from ec2.service import get_instances_service, launch_instance_service
+from ec2.schemas import LaunchInstanceRequest
 
 router = APIRouter()
 
@@ -13,3 +14,18 @@ async def get_instances():
         "message": message
     }
     return JSONResponse(status_code=200,content=content)
+
+
+@router.post("/instances")
+async def launch_instance(payload: LaunchInstanceRequest):
+    instance_name = payload.name
+    launch_template= payload.launch_template
+    status, message = launch_instance_service(instance_name,launch_template)
+    content = {
+        "status": status,
+        "message": message
+    }
+    if status != "error":
+        return JSONResponse(status_code=201, content=content)
+    else:
+        return JSONResponse(status_code=500, content=content)
--- ec2/service.py
+++ ec2/service.py
@@ -1,7 +1,7 @@
 import boto3
 
 
-ec2 = boto3.client("ec2")
+ec2 = boto3.client("ec2",region_name="ap-south-1")
 ec2_resource = boto3.resource("ec2",region_name="ap-south-1")
 
 
@@ -15,3 +15,28 @@ def get_instances_service():
         }
         instance_list.append(d)
     return "success",instance_list
+
+
+def launch_instance_service(instance_name,launch_template):
+    response = ec2.run_instances(
+        LaunchTemplate={
+            'LaunchTemplateName': launch_template,
+            'Version': '$Latest'
+        },
+        MinCount = 1,
+        MaxCount = 1,
+        # Defining the instance name below
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': instance_name  # This becomes the display name
+                    },
+                ]
+            },
+        ]
+    )
+    instance_id = response['Instances'][0]['InstanceId']
+    return "success", instance_id
