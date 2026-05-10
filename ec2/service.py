6400c4027a8f920c2f9746ac4922fb21a641095b
thisisnikhar <nikharsachdeva14@gmail.com>
2026-05-10 16:27:46 +0530

    Resolved stash conflict in ec2 service

---
+++ ec2/service.py
@@ -0,0 +1,84 @@
+import boto3
+from datetime import datetime
+from botocore.exceptions import ClientError
+
+
+ec2 = boto3.client("ec2",region_name="ap-south-1")
+ec2_resource = boto3.resource("ec2",region_name="ap-south-1")
+
+
+def launch_instance_service(instance_name,launch_template):
+    try:
+        response = ec2.run_instances(
+            LaunchTemplate={
+                'LaunchTemplateName': launch_template,
+                'Version': '$Latest'
+            },
+            MinCount = 1,
+            MaxCount = 1,
+            # Defining the instance name below
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': instance_name  # This becomes the display name
+                        },
+                    ]
+                },
+            ]
+        )
+        instance_id = response['Instances'][0]['InstanceId']
+        return "success", instance_id
+    except ClientError as e:
+        return "error",str(e.args[0])
+
+
+def get_instances_service():
+    instance_list = []
+    try:
+        for instance in ec2_resource.instances.all():
+            d = {
+                "instance_id": instance.id,
+                "instance_name":next((tag['Value'] for tag in instance.tags if tag['Key'] == 'Name'), 'N/A'),
+                "instance_state": instance.state['Name']
+            }
+            instance_list.append(d)
+
+        return "success", instance_list
+    except ClientError as e:
+        return "error",str(e.args[0])
+
+
+def get_instances_by_state_service(state):
+    try:
+        instances = ec2_resource.instances.filter(
+            Filters=[{
+                'Name': 'instance-state-name',
+                'Values': [state]
+            }]
+        )
+        instances_list = []
+        for instance in instances:
+            instances_list.append(
+                {
+                    "instance_id": instance.id,
+                    "instance_type": instance.instance_type,
+                    "state": instance.state['Name'],
+                    "launch_time": "ISO-"+str(datetime.isoformat(instance.launch_time)),
+                    "public_ip": instance.public_ip_address,
+                    "private_ip": instance.private_ip_address
+                }
+            )
+        return "success", instances_list
+    except ClientError as e:
+        return "error",str(e.args[0])
+
+
+def terminate_instance_by_id_service(instance_ids):
+    try:
+        response = ec2.terminate_instances(InstanceIds=instance_ids)
+        return "success",f"Instance with id {instance_ids} terminated"
+    except ClientError as e:
+        return "error",str(e.args[0])
